f4061abcd4a3700b115dc94593d3f350ba1e3837
Narendra Bhojanala <nbhojanala@delaplex.com>
2022-12-23 14:07:04 +0530

    Initial Commit

--- springboot-two-way-ssl-client-rest/src/main/java/com/scb/client/controller/JsonToXmlController.java
+++ springboot-two-way-ssl-client-rest/src/main/java/com/scb/client/controller/JsonToXmlController.java
@@ -1,9 +1,4 @@
-/* ***************************************************************************************
- * (c) COPYRIGHT Kuecker Pulse Integration, L.P. 2022 All Rights Reserved
- * No part of this copyrighted work may be reproduced, modified, or distributed
- * in any form or by any means without the prior written permission of Kuecker Pulse
- * Integration, L.P.
- ****************************************************************************************/
+
 package com.scb.client.controller;
 
 import java.io.File;
